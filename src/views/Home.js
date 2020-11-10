af4cc688548367f01c9824aa51ae387280951dcb
juli <nani.jimenez@hotmail.com>
2020-11-10 12:08:19 -0500

    pp

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import '../views/css/Home.css';
-import background from '../img/background.jpg'
+import { Link } from "react-router-dom";
+import logo from '../img/logo.png'
 
 
 const Home = () => {
   return (
     <div className="Home">
-      <div className="Background">
-      <img src={background} alt="hello"  />
+      <div className="Logo">
+     
+      <img src={logo} alt="logo"  /> </div>
       </div>
+
+      <div className='Buttons'>
+            <button className="Buttons">
+            <Link to={'/Mesero'}><h2>Mesero</h2></Link> </button>
+           
+            <button className="Buttons">
+            <Link to={'/Cocinero'}><h2>Cocinero</h2></Link></button> 
       </div>
-   
-  );
+    
+);
 }
 
 export default Home;
