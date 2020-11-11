aab748bec49f07a1306255848cccea8a44447c28
juli <nani.jimenez@hotmail.com>
2020-11-11 11:20:36 -0500

    router

--- src/components/Router.js
+++ src/components/Router.js
@@ -6,6 +6,8 @@ import { Switch, Route } from 'react-router-dom';
 /*Components*/
 import Home from '../views/Home';
 import notFound from "../components/notFound";
+import Waiter from '../views/waiter';
+import Chef from '../views/chef'
 
 
 
@@ -13,7 +15,9 @@ const Router = () => {
     return (
 <Switch>
     <Route exact path='/' component={Home}/>
-    
+    <Route exact path='/mesero' component={Waiter}/>
+    <Route exact path='/cocinero' component={Chef}/>
+
 
     <Route component={notFound} />
 
---
+++ src/views/chef.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Chef = () => { 
+
+    return (
+      
+  <div> {console.log('holiii')} </div>
+    )
+}
+
+export default Chef;
